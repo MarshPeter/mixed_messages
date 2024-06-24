1e7aed1f21cdb33ac0c3f797c451e1d6376c2069
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-06-24 23:44:12 +1000

    collected bravery quotes

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const perserveranceQuotes = [
     "Courage doesn't always roar. Sometimes courage is the quiet voice at the end of the day saying 'I will try again tomorrow.\n\t--Mary Anne Radmacher, American author and artist.",
-    "Many of life's failures are people who did not realize how close they were to success when they gave up.\n\t--Thomas Edison (1874-1931), inventor of the light bulb",
+    "Many of life's failures are people who did not realize how close they were to success when they gave up.\n\t--Thomas Edison (1874-1931), inventor of the light bulb.",
     "Failure is only the opportunity to begin again, this time more intelligently.\n\t--Henry Ford (1863-1947), founder of Ford Motor Company",
     "Fall seven times and stand up eight.\n\t--Japanese Proverb.",
     "It's not that I'm so smart, it's just that I stay with problems longer.\n\t--Albert Einstein (1879-1955), physicist and developer of the theory of relativity.",
-    " A failure is not always a mistake. It may simply be the best one can do under the circumstances. The real mistake is to stop trying.\n\t--B.F. Skinner (1904-1990), American psychologist",
-    "Ask yourself this question: 'Will this matter a year from now?'\n\t--Richard Carlson, American psychotherapist and author of Don't Sweat the Small Stuff",
-    "What if I told you that 10 years from now, your life would be exactly the same? I doubt you'd be happy. So, why are you so afraid of change?\n\t--Karen Salmansohn, best-selling self-help author",
-    "As I look back on my life, I realize that every time I thought I was being rejected from something good, I was actually being redirected to something better.\n\t--Dr. Steve Maraboli, speaker and author",
-    "Success is the sum of small efforts, repeated day in and day out.\n\tRobert Collier (1885-1950), American self-help author",
+    " A failure is not always a mistake. It may simply be the best one can do under the circumstances. The real mistake is to stop trying.\n\t--B.F. Skinner (1904-1990), American psychologist.",
+    "Ask yourself this question: 'Will this matter a year from now?'\n\t--Richard Carlson, American psychotherapist and author of Don't Sweat the Small Stuff.",
+    "What if I told you that 10 years from now, your life would be exactly the same? I doubt you'd be happy. So, why are you so afraid of change?\n\t--Karen Salmansohn, best-selling self-help author.",
+    "As I look back on my life, I realize that every time I thought I was being rejected from something good, I was actually being redirected to something better.\n\t--Dr. Steve Maraboli, speaker and author.",
 ];
 
-const braveryQuotes = [];
+const braveryQuotes = [
+    "Fear cuts deeper than swords.\n\t-- George R.R. Martin, A Game of Thrones.",
+    "Don't be afraid of your fears. They're not there to scare you. They're there to let you know that something is worth it.\n\t-- C. JoyBell C.",
+    "you can, you should, and if you’re brave enough to start, you will.\n\t--Stephen King, On Writing: A Memoir of the Craft.",
+    "Scared is what you're feeling. Brave is what you're doing.\n\t--Emma Donoghue, Room.",
+    "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.\n\t--Nelson Mandela.",
+    "It is hard to fail, but it is worse never to have tried to succeed.\n\t--Theodore Roosevelt.",
+    "The biggest adventure you can ever take is to live the life of your dreams.\n\t--Oprah Winfrey.",
+    "Every person has the power to change their fate if they are brave enough to fight for what they desire more than anything.\n\t--Stephanie Garber, Caraval",
+];
 
 const wisdomQuotes = [];
